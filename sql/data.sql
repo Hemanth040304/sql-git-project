1d3b75396165bcfde6a1da00eb762bccd25c4afa
Hemanth040304 <hemanthpannasa@gmail.com>
2025-11-06 01:21:49 +0000

    added data

---
+++ sql/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO author (name, country) VALUES
+('J.K. Rowling', 'United Kingdom'),
+('George R.R. Martin', 'United States'),
+('Haruki Murakami', 'Japan');
+
+INSERT INTO books(title, price, author_id) VALUES
+('1984', 15.99, 1),
+('Harry Potter', 25.50, 2),
+('Kafka on the Shore', 19.75, 3);
